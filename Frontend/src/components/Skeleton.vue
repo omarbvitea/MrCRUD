a9edef01ad0acb5e8c7c14c1ef08cb2ab7e14765
llViteOmarll <viteomar537.b@gmail.com>
2025-01-25 23:36:07 -0500

    feat: responsive skeleton

--- Frontend/src/components/Skeleton.vue
+++ Frontend/src/components/Skeleton.vue
@@ -5,8 +5,8 @@
 				<div class="skeleton h-12 w-12 shrink-0 rounded-full"></div>
 				<div class="skeleton h-8 w-20"></div>
 			</div>
-			<div class="skeleton h-8 w-60"></div>
-			<div class="skeleton h-8 w-20"></div>
+			<div class="skeleton h-8 w-40 md:w-60 hidden sm:flex"></div>
+			<div class="skeleton h-8 w-20 hidden sm:flex"></div>
 			<div class="flex items-center gap-4">
 				<div class="skeleton h-10 w-10 shrink-0 rounded-full"></div>
 				<div class="skeleton h-10 w-10 shrink-0 rounded-full"></div>
